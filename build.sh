44915d15fdcfe88ceea5768260190ce6eb123a8f
Lukasz Szczesny <luk@mimuw.edu.pl>
2014-04-28 19:37:52 +0200

    Refactoring

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+[[ -z $DEBUG ]] || set -o xtrace
 
-[[ -z $DEBUG ]] || set -x
+set -o nounset
+set -o errexit
 
-DEBIAN_VERSION="wheezy"
-DEBIAN_ARCH="i386"
-DEBIAN_MIRROR="http://ftp.pl.debian.org"
-
-# --
+DEBIAN_VERSION="${DEBIAN_VERSION:-wheezy}"
+DEBIAN_ARCH="${DEBIAN_ARCH:-i386}"
+DEBIAN_MIRROR="${DEBIAN_MIRROR:-http://ftp.pl.debian.org}"
 
 TEMP="$( mktemp -d )"
 VM_NAME="${DEBIAN_VERSION}-${DEBIAN_ARCH}-$( date +'%Y%m%d%H%M%S' )"
@@ -16,9 +15,17 @@ NETBOOT_URL="${DEBIAN_MIRROR}/debian/dists/${DEBIAN_VERSION}/main/installer-${DE
 PXE_TFTP_PREFIX="${TEMP}/boot"
 PXE_BOOT_FILE="pxelinux.0"
 
+
+log() {
+	local prefix="[$(date +%Y/%m/%d\ %H:%M:%S)]:"
+	echo "${prefix} $@" >&2
+} 
+
 trap 'rm -rf "${TEMP}" "${VM_NAME}"; exit $?' EXIT INT TERM
 
 mkdir -p ${PXE_TFTP_PREFIX}
+
+log "INFO" "Preparing netboot image."
 
 NETBOOT_SHA256SUM="$( wget -q ${NETBOOT_URL}/SHA256SUMS -O - \
 	| grep 'netboot/netboot.tar.gz' \
@@ -37,8 +44,8 @@ tar xfz "${TEMP}/netboot.tar.gz" -C "${PXE_TFTP_PREFIX}"
 
 gunzip "${PXE_TFTP_PREFIX}/debian-installer/${DEBIAN_ARCH}/initrd.gz"
 
-echo "preseed.cfg" | cpio -ovA -H newc -F "${PXE_TFTP_PREFIX}/debian-installer/${DEBIAN_ARCH}/initrd"
-echo "late_command.sh" | cpio -ovA -H newc -F "${PXE_TFTP_PREFIX}/debian-installer/${DEBIAN_ARCH}/initrd" 
+echo "preseed.cfg" | cpio -ovA -H newc -F "${PXE_TFTP_PREFIX}/debian-installer/${DEBIAN_ARCH}/initrd" >/dev/null 2>&1
+echo "late_command.sh" | cpio -ovA -H newc -F "${PXE_TFTP_PREFIX}/debian-installer/${DEBIAN_ARCH}/initrd" >/dev/null 2>&1 
 
 gzip "${PXE_TFTP_PREFIX}/debian-installer/${DEBIAN_ARCH}/initrd"
 
@@ -51,10 +58,12 @@ label auto
 	append priority=critical file=/preseed.cfg hostname=${VM_NAME} keymap=pl locale=pl_PL auto vga=788 initrd=debian-installer/${DEBIAN_ARCH}/initrd.gz
 EOF
 
+log "INFO" "Creating VM."
+
 VBoxManage createvm \
 	--name "${VM_NAME}" \
 	--ostype Debian_64 \
-	--register
+	--register >/dev/null 2>&1
 
 VBoxManage modifyvm "${VM_NAME}" \
 	--memory 512 \
@@ -62,40 +71,47 @@ VBoxManage modifyvm "${VM_NAME}" \
 	--nictype1 Am79C970A \
 	--boot1 net \
 	--vram 16 \
-	--pae off
+	--pae off >/dev/null 2>&1
 
 VBoxManage storagectl "${VM_NAME}" \
 	--name "SATA Controller" \
 	--add sata \
 	--controller IntelAhci \
-	--portcount 1
+	--portcount 1 >/dev/null 2>&1
 
 VBoxManage createhd \
 	--filename "${VM_NAME}/${VM_NAME}.vdi" \
-	--size 32768
+	--size 32768 >/dev/null 2>&1
 
 VBoxManage setextradata "${VM_NAME}" \
 	"VBoxInternal/Devices/pcnet/0/LUN#0/Config/TFTPPrefix" \
-	"${PXE_TFTP_PREFIX}"
+	"${PXE_TFTP_PREFIX}" >/dev/null 2>&1
 
 VBoxManage setextradata "${VM_NAME}" \
 	"VBoxInternal/Devices/pcnet/0/LUN#0/Config/BootFile" \
-	"$PXE_BOOT_FILE"
+	"$PXE_BOOT_FILE" >/dev/null 2>&1
 
 VBoxManage storageattach "${VM_NAME}" \
 	--storagectl "SATA Controller" \
 	--port 0 \
 	--type hdd \
-	--medium "${VM_NAME}/${VM_NAME}.vdi"
+	--medium "${VM_NAME}/${VM_NAME}.vdi" >/dev/null 2>&1
+
+log "INFO" "Booting VM. This may take a few minutes..."
 
 VBoxHeadless -s "${VM_NAME}" \
-	-v off
+	-v off >/dev/null 2>&1
 
 VBoxManage modifyvm "${VM_NAME}" \
-	--boot1 disk
+	--boot1 disk >/dev/null 2>&1
 
-vagrant package --base "${VM_NAME}" --output "${VM_NAME}.box"
+log "INFO" "Creating vagrant box."
 
-VBoxManage unregistervm "${VM_NAME}" --delete
+vagrant package --base "${VM_NAME}" --output "${VM_NAME}.box" >/dev/null 2>&1
 
+log "INFO" "Cleaning up."
+
+VBoxManage unregistervm "${VM_NAME}" --delete >/dev/null 2>&1
+
+log "INFO" "Done."
 
--- late_command.sh
+++ late_command.sh
@@ -31,13 +31,13 @@ if has("autocmd")
   filetype plugin indent on
 endif
 
-set showcmd		" Show (partial) command in status line.
-set showmatch		" Show matching brackets.
-set ignorecase		" Do case insensitive matching
-set smartcase		" Do smart case matching
-set incsearch		" Incremental search
-set autowrite		" Automatically save before commands like :next and :make
-set hidden              " Hide buffers when they are abandoned
+set showcmd
+set showmatch
+set ignorecase
+set smartcase
+set incsearch
+set autowrite
+set hidden
 EOV
 
 # ...and /root/.bashrc
